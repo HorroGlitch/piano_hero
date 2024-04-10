386006105eef10636c04eab2c758ba208ab9d291
HorroGlitch <lverot@edenschool.fr>
2024-04-10 09:51:39 +0200

    create white and blacdk div

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+let allTheTiles = document.querySelector(".allTheTiles");
+
+  const whiteTile = document.createElement("div");
+  whiteTile.classList.add("white");
+  allTheTiles.appendChild(whiteTile);
+
+  const blackTile = document.createElement("div");
+  blackTile.classList.add("black");
+  whiteTile.appendChild(blackTile);
